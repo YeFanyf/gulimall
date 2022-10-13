1fe4cef7b0d71079d1cfed76dcd183f7c6fd3229
yefan <2394437179@qq.com>
2022-10-14 00:09:36 +0800

    项目初始curd搭建

--- gulimall-product/src/test/java/com/yf/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/yf/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,25 @@
 package com.yf.gulimall.product;
 
+import com.yf.common.utils.PageUtils;
+import com.yf.gulimall.product.entity.BrandEntity;
+import com.yf.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
+
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        BrandEntity brandEntity = new BrandEntity();
+
+        brandEntity.setName("yefan");
+        brandService.save(brandEntity);
+        System.out.println("====");
     }
 
 }
